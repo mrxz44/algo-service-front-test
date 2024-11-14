d212ac4d7060189cad3a38e2921a673b5098c802
Mikhail NovikEsmenov <mikhail.novikesmenov@exness.com>
2024-11-14 14:25:35 +0200

    var edit

--- .vitepress/axios.js
+++ .vitepress/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VUE_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   // baseURL: 'https://algo-service-backend-production.up.railway.app/',
 });
 
