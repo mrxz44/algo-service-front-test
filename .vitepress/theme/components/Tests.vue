7644af39e0eb3da1fa0006739ca54bb51f9a9512
Mikhail NovikEsmenov <mikhail.novikesmenov@exness.com>
2024-11-14 14:44:09 +0200

    cleaned tests.vue

--- .vitepress/theme/components/Tests.vue
+++ .vitepress/theme/components/Tests.vue
@@ -2,7 +2,7 @@
 import { onMounted, ref } from 'vue'
 import SiteMap from './SiteMap.vue'
 // import NewsLetter from './NewsLetter.vue'
-import { load, data, base } from './sponsors'
+// import { load, data, base } from './sponsors'
 // import SponsorsGroup from './SponsorsGroup.vue'
 import VueMasteryModal from './VueMasteryModal.vue'
 import api from '../../axios'
@@ -30,24 +30,13 @@ const loadApiData = async () => {
 // onMounted(load)
 onMounted(() => {
   loadApiData()
-  load()
+  // load()
 })
 </script>
 
 
 <template>
   <section id="hero">
-    <img id="uwu" alt="Vue.js Kawaii Logo by @icarusgkx" />
-    <h1 class="tagline">
-      <span class="accent">Sure </span>
-      Way to
-      <span class="accent">Trade</span>
-
-      <!--      <br />For Algo Trading-->
-    </h1>
-    <p class="description">
-      Unlock professional trading algorithms and market insights for consistent success
-    </p>
 
     <p v-if="apiData" class="api-output">
       <strong>API Output:</strong> {{ apiData.message }}
@@ -55,36 +44,6 @@ onMounted(() => {
 
     <p>{{ apiBaseUrl }}</p>
 
-    <p class="actions">
-      <VueMasteryModal />
-      <a class="get-started" href="/guide/introduction.html">
-        Get Started
-        <svg
-          class="icon"
-          xmlns="http://www.w3.org/2000/svg"
-          width="10"
-          height="10"
-          viewBox="0 0 24 24"
-        >
-          <path
-            d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"
-          />
-        </svg>
-      </a>
-      <a class="setup" href="/guide/quick-start.html">Install</a>
-      <a class="security" href="https://v2.vuejs.org/eol/" target="_blank">
-        Try free tier
-        <svg
-          class="icon"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 512 512"
-        >
-          <path
-            d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"
-          />
-        </svg>
-      </a>
-    </p>
   </section>
 
 
